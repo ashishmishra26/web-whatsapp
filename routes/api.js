39218edf6b3afea18c273559cdd007758b81d720
Ashish Mishra <ashish@fusioncharts.com>
2019-03-04 10:17:44 +0530

    changed post route path

--- routes/api.js
+++ routes/api.js
@@ -10,7 +10,7 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
-    mongoose.connect(process.env.DB_URL || 'mongodb://localhost/shipmentDashboardDB');
+    mongoose.connect(process.env.DB_URL);
     Conversation.insertMany(req.body, (err, docs) => {
         console.log(err);
         mongoose.connection.close();
